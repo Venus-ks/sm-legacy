989a8f428a7bf2e49930ba9bc8045f0d2d23bf88
hjshyo <hjshyo@hakjisa.co.kr>
2021-11-05 16:23:04 +0900

    FIX reviewer attach file valiable

--- admin/widget/reviewfile.php
+++ admin/widget/reviewfile.php
@@ -3,16 +3,16 @@
     <td>
         <input type="file" name="review_file" id="review_file" style="width:100%;" required/>
         <div style="padding-top:5px;">
-            <?php if ($data['step'] == '4' || $data['step'] == '3'):?>
-                <a href="<?=$info['review_form1_url']?>" class="btn btn-danger" style="color:#FFF">
+            <?php if ($data['step'] == '4' || $data['step'] == '3' || !$data['step']):?>
+                <a href="/down.php?link=<?=$info['file']['review_form1']['link']?>" class="btn btn-danger" style="color:#FFF">
                     <span class="glyphicon glyphicon-paperclip"></span> 1차 심사의견서
                 </a>
             <?php elseif ($data['step'] == '14'):?>
-                <a href="<?=$info['review_form2_url']?>" class="btn btn-danger" style="color:#FFF">
+                <a href="/down.php?link=<?=$info['file']['review_form2']['link']?>" class="btn btn-danger" style="color:#FFF">
                     <span class="glyphicon glyphicon-paperclip"></span> 2차 심사의견서
                 </a>
             <?php elseif ($data['step'] == '24'):?>
-                <a href="<?=$info['review_form3_url']?>" class="btn btn-danger" style="color:#FFF">
+                <a href="/down.php?link=<?=$info['file']['review_form3']['link']?>" class="btn btn-danger" style="color:#FFF">
                     <span class="glyphicon glyphicon-paperclip"></span> 3차 심사의견서
                 </a>
             <?php endif ?>
