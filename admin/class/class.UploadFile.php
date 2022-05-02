e6c7704a876bca17b49cc5a8a424385a2c5ad975
ks <kscho@hakjisa.co.kr>
2022-05-02 15:03:59 +0900

    Modify Upload And Pulisher Setting

--- admin/class/class.UploadFile.php
+++ admin/class/class.UploadFile.php
@@ -26,10 +26,10 @@ class UploadFile
 		$filesize  = $uploaded_file['size'];
 		$filename  = $uploaded_file['name'];
 		$filename  = preg_replace('/[^a-zA-Z0-9가-힣.]/', '_', $filename);
-		$rfilename	= iconv("utf-8", "euc-kr", $filename);
+		$rfilename	= iconv("UTF-8", "UTF-8", $filename);
 		//중복 파일 방지를 위해 타임스탬프를 붙인다.
 		$dest_file = "../data/{$type}/".$rfilename;
-		$sfilename = iconv("euc-kr", "utf-8", $rfilename);
+		$sfilename = iconv("UTF-8", "UTF-8", $rfilename);
 		if (is_uploaded_file($tmp_file)){
 			$error_code = move_uploaded_file($tmp_file, $dest_file) or die($uploaded_file['error']);
 		}
