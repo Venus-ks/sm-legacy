b7ead7d0341368f99082f719b17d77b2f33c7f31
ks <kscho@hakjisa.co.kr>
2022-07-05 14:00:51 +0900

    class Upload and file name Unicode => UTF-8 Change

--- admin/class/class.UploadFile.php
+++ admin/class/class.UploadFile.php
@@ -6,14 +6,16 @@ class UploadFile
 		$tmp_file  = $uploaded_file['tmp_name'];
 		$filesize  = $uploaded_file['size'];
 		$filename  = $uploaded_file['name'];
-		$filename  = preg_replace('/[^a-zA-Z0-9가-힣.]/', '_', $filename);
-		$rfilename	= iconv("utf-8", "euc-kr", $filename);
+		$filename  = preg_replace('/[^a-zA-Z0-9가-힣.]/u', '_', $filename);
+		// $rfilename	= iconv("utf-8", "euc-kr", $filename);
+		$rfilename = $filename;
 		//중복 파일 방지를 위해 타임스탬프를 붙인다.
 		$mcrtime = explode(' ',microtime());
 		$mcrtime[0] = substr($mcrtime[0],2,6);
 		$rfilename = time().$mcrtime[0]."^".$rfilename;
 		$dest_file = "../data/{$type}/".$rfilename;
-		$sfilename = iconv("euc-kr", "utf-8", $rfilename);
+		// $sfilename = iconv("euc-kr", "utf-8", $rfilename);
+		$sfilename = $rfilename;
 		if (is_uploaded_file($tmp_file)){
 			$error_code = move_uploaded_file($tmp_file, $dest_file) or die($uploaded_file['error']);
 		}
@@ -25,11 +27,13 @@ class UploadFile
 		$tmp_file  = $uploaded_file['tmp_name'];
 		$filesize  = $uploaded_file['size'];
 		$filename  = $uploaded_file['name'];
-		$filename  = preg_replace('/[^a-zA-Z0-9가-힣.]/', '_', $filename);
-		$rfilename	= iconv("UTF-8", "UTF-8", $filename);
+		$filename  = preg_replace('/[^a-zA-Z0-9가-힣.]/u', '_', $filename);
+		// $rfilename	= iconv("UTF-8", "UTF-8", $filename);
+		$rfilename = $filename;
 		//중복 파일 방지를 위해 타임스탬프를 붙인다.
 		$dest_file = "../data/{$type}/".$rfilename;
-		$sfilename = iconv("UTF-8", "UTF-8", $rfilename);
+		// $sfilename = iconv("UTF-8", "UTF-8", $rfilename);
+		$sfilename = $rfilename;
 		if (is_uploaded_file($tmp_file)){
 			$error_code = move_uploaded_file($tmp_file, $dest_file) or die($uploaded_file['error']);
 		}
